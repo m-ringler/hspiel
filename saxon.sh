f2dd127122f3205b9597ab0b8317e0cf75fa9d5d
m-ringler <42344272+m-ringler@users.noreply.github.com>
2021-11-18 18:45:58 +0100

    Update saxon.sh

--- saxon.sh
+++ saxon.sh
@@ -1,4 +1,4 @@
-SAXON="java -jar /home/ringler/java/saxonb9-1-0-5j/saxon9.jar"
+SAXON="java -jar saxonb9-1-0-5j/saxon9.jar"
 $SAXON hspiel.xml hspiel.xsl
 $SAXON hspiel.xml hspiel.xsl frames=yes
 $SAXON hspiel.xml mplayer.xsl
